646cfcef23fa83e8e78cf2c1df5a21426f26ca58
Victor Emanuel Souza de Oliveira <129910747+svictorem@users.noreply.github.com>
2026-02-20 17:57:36 -0300

    Exer_Classe

---
+++ orientacao_a_objeto_classe.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+</head>
+<body>
+    <?php 
+    //definindo uma classe
+    class Carro {
+        //ojetos atributos
+        public $marca;
+        public $modelo;
+        public $ano;
+        public $cor;
+        public $velocidade =0;
+
+        //metodos
+        public function acelerar(){
+            $this -> velocidade >=10;
+            echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h";
+        }
+
+        //segundo metodo
+        public function frear(){
+            if($this -> velocidade >=10){
+                $this->velocidade <=10;
+            }
+            echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h";
+        }
+
+    }
+    
+    
+    
+    ?>
+    
+</body>
+</html>
