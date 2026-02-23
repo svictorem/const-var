21d2141ad95337977fc007df5e250abfd92b5f1c
Victor Emanuel <129910747+svictorem@users.noreply.github.com>
2026-02-23 17:31:20 -0300

    Update orientacao_a_objeto_classe.php

--- orientacao_a_objeto_classe.php
+++ orientacao_a_objeto_classe.php
@@ -8,31 +8,38 @@
 <body>
     <?php 
     //definindo uma classe
-    class Carro {
-        //ojetos atributos
-        public $marca;
-        public $modelo;
-        public $ano;
-        public $cor;
-        public $velocidade =0;
+        class Carro {
+            //ojetos atributos
+            public $marca;
+            public $modelo;
+            public $ano;
+            public $cor;
+            public $velocidade =0;
 
-        //metodos
-        public function acelerar(){
-            $this -> velocidade >=10;
-            echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h";
-        }
-
-        //segundo metodo
-        public function frear(){
-            if($this -> velocidade >=10){
-                $this->velocidade <=10;
+            //metodos
+            public function acelerar(){
+                $this -> velocidade >=10;
+                echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h";
             }
-            echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h";
-        }
 
-    }
-    
-    
+            //segundo metodo
+            public function frear(){
+                if($this -> velocidade >=10){
+                    $this->velocidade -=10;
+                }else{
+                    $this->velocidade = 0;
+                }
+                echo "Velociade e igual: " . $this->velocidade . "Km/h";
+            }
+            
+        }
+        
+        $meuCarro = new Carro();
+        $meuCarro->marca = "Toyota";
+        $meuCarro->acelerar(); // Velocidade vai para 10
+        $meuCarro->acelerar(); // Velocidade vai para 20
+        $meuCarro->frear();    // Velocidade volta para 10
+
     
     ?>
     
